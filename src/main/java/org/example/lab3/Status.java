47bc6c6e3cded67c88f9d9a717134d080d5a056d
Szymon <szymon.glomski@gmail.com>
2023-10-27 13:39:45 +0200

    lock + condition

--- src/main/java/org/example/lab3/Status.java
+++ src/main/java/org/example/lab3/Status.java
@@ -1,29 +1,40 @@
 package org.example.lab3;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Status {
+    ReentrantLock lock = new ReentrantLock();
+    Condition notEmpty = lock.newCondition();
+    Condition notFull = lock.newCondition();
     private int counter = 0;
-    public synchronized void setToReady() {
-        while (counter == 1){
+    final int limit = 10;
+    public void setToReady() {
+        lock.lock();
+        while (counter >= limit){
             try {
-                wait();
+                notFull.await();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
-        counter = 1;
-        System.out.println("Produce ");
-        notifyAll();
+        counter++;
+        System.out.println("Produce "+counter);
+        notEmpty.signal();
+        lock.unlock();
     }
-    public synchronized void setToTaken() {
-        while (counter == 0){
+    public void setToTaken() {
+        lock.lock();
+        while (counter <= 0){
             try {
-                wait();
+                notEmpty.await();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
-        counter = 0;
-        System.out.println("Consume ");
-        notifyAll();
+        counter--;
+        System.out.println("Consume "+counter);
+        notFull.signal();
+        lock.unlock();
     }
 }
